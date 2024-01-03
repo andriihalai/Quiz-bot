773ef5b31a21628568e0e0a82d374224dcc6d270
AndriiHalai <a.halai.im21@kpi.ua>
2024-01-03 11:59:54 +0200

    Add start command and error handling

--- index.js
+++ index.js
@@ -2,12 +2,32 @@
 
 require("dotenv").config();
 
-const { Bot } = require("grammy");
+const { Bot, Keyboard, GrammyError, HttpError } = require("grammy");
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
-bot.command("start", (ctx) => {
-  console.log(ctx);
+bot.command("start", async (ctx) => {
+  const menuButtons = [
+    ["Українська мова", "Математика"],
+    ["Історія України", "Англійська мова"],
+  ];
+  const keyboard = Keyboard.from(menuButtons).resized();
+  await ctx.reply("Обери предмет", {
+    reply_markup: keyboard,
+  });
+});
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
+  }
 });
 
 bot.start();
