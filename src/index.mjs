30405ff2cc72249ace996819824426ba71a38e80
AndriiHalai <a.halai.im21@kpi.ua>
2024-01-05 23:12:26 +0200

    Change inline keyboard to simple keyboard

--- src/index.mjs
+++ src/index.mjs
@@ -9,6 +9,8 @@ import { pool as db } from "./db.js";
 
 const bot = new Bot(BOT_API_KEY);
 
+const RETURN_TO_MENU = "Повернутися до головного меню";
+
 const getSubjects = async () => {
   try {
     const result = [];
@@ -20,37 +22,49 @@ const getSubjects = async () => {
   } catch (e) {
     console.error(e);
   }
-}
+};
 
-const subjects = await getSubjects();
-
-bot.command("start", async (ctx) => {
+const showMenu = async (ctx) => {
   try {
-    const menuButtons = subjects.map(subject => [subject]);
+    const menuButtons = subjects.map((subject) => [subject]);
 
     const keyboard = Keyboard.from(menuButtons).resized();
-    await ctx.reply("Обери предмет", {
+    await ctx.reply("Оберіть предмет", {
       reply_markup: keyboard,
     });
   } catch (e) {
     console.error(e);
   }
-});
+};
+
+const subjects = await getSubjects();
+
+bot.command("menu", showMenu);
+
+bot.hears(RETURN_TO_MENU, showMenu);
 
 bot.hears(subjects, async (ctx) => {
   try {
     const subject = ctx.message.text;
-    const { id: subjectId } = (await db.query("SELECT id FROM subjects WHERE subject = $1", [subject])).rows[0];
-    const testTypes = (await db.query("SELECT test_type, description FROM test_types WHERE subject_id = $1", [subjectId])).rows;
+    const { id: subjectId } = (
+      await db.query("SELECT id FROM subjects WHERE subject = $1", [subject])
+    ).rows[0];
+    const testTypes = (
+      await db.query(
+        "SELECT test_type, description FROM test_types WHERE subject_id = $1",
+        [subjectId]
+      )
+    ).rows;
     const rows = [];
-  
+
     for (const item of testTypes) {
-      const buttonRow = InlineKeyboard.text(item.test_type, item.description);
-      rows.push([buttonRow]);
+      rows.push([item.test_type]);
     }
-  
-    const keyboard = InlineKeyboard.from(rows);
-  
+
+    rows.push([RETURN_TO_MENU]);
+
+    const keyboard = Keyboard.from(rows).resized();
+
     await ctx.reply("Що саме ви хочете попрактикувати?", {
       reply_markup: keyboard,
     });
@@ -59,7 +73,10 @@ bot.hears(subjects, async (ctx) => {
   }
 });
 
-bot.hears("callback_query:data", async (ctx) => {});
+bot.on("callback_query:data", async (ctx) => {
+  await ctx.reply(ctx.callbackQuery.data);
+  await ctx.answerCallbackQuery();
+});
 
 bot.catch((err) => {
   handleErrors(err);
